5bf481c9adcab29bac188ebfc9904f28939e0826
Jakub Cisło <cubix651@gmail.com>
2015-04-23 00:57:05 +0200

    Created hierarchy of files and empty form

---
+++ src/main/java/com/note/quarter/MainForm.java
@@ -0,0 +1,22 @@
+package com.note.quarter;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainForm extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("MainForm.fxml"));
+        primaryStage.setTitle("QuarterNote");
+        Scene scene = new Scene(root, 300, 275);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/test/java/com/note/quarter/MainTest.java
@@ -0,0 +1,4 @@
+package com.note.quarter;
+
+public class MainTest {
+}
