e53e18d62f0950642a2631e8c5607edce5a6d0bc
Jakub Cisło <cubix651@gmail.com>
2015-04-27 18:02:09 +0200

    Added simple implementation of MelodyPlayer

---
+++ src/main/java/com/note/quarter/MelodyPlayer.java
@@ -0,0 +1,40 @@
+package com.note.quarter;
+
+import javax.sound.midi.MidiChannel;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.MidiUnavailableException;
+import javax.sound.midi.Synthesizer;
+
+public class MelodyPlayer {
+
+    private int volume = 80;
+    private Synthesizer synthesizer;
+    private MidiChannel mainChannel;
+    final private int mainChannelNumber = 0;
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public MelodyPlayer() throws MidiUnavailableException{
+        synthesizer = MidiSystem.getSynthesizer();
+        synthesizer.open();
+        mainChannel = synthesizer.getChannels()[mainChannelNumber];
+    }
+
+    public void noteOn(int noteNumber) {
+        mainChannel.noteOn(noteNumber, volume);
+    }
+
+    public void noteOff(int noteNumber) {
+        mainChannel.noteOff(noteNumber);
+    }
+
+    public void allNotesOff() {
+        mainChannel.allNotesOff();
+    }
+}
