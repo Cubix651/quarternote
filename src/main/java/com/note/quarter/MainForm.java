c1f4407a69949694a691f0da85e5cc204b5efd38
Jakub Cisło <cubix651@gmail.com>
2015-06-10 20:00:15 +0200

    Fix jar problems

--- src/main/java/com/note/quarter/MainForm.java
+++ src/main/java/com/note/quarter/MainForm.java
@@ -12,16 +12,27 @@ public class MainForm extends Application {
     private FXMLLoader fxmlLoader;
     @Override
     public void start(Stage primaryStage) throws Exception {
+        System.out.println(1);
         fxmlLoader = new FXMLLoader();
+        System.out.println(2);
         fxmlLoader.setLocation(getClass().getResource("MainForm.fxml"));
+        System.out.println(3);
         fxmlLoader.setBuilderFactory(new JavaFXBuilderFactory());
+        System.out.println(4);
         Parent root = fxmlLoader.load();
+        System.out.println(5);
 
+        System.out.println(6);
         primaryStage.setTitle("QuarterNote");
+        System.out.println(7);
         Scene scene = new Scene(root, 1100, 600);
+        System.out.println(8);
 
+        System.out.println(9);
         primaryStage.setScene(scene);
+        System.out.println(10);
         primaryStage.show();
+        System.out.println(11);
     }
 
     @Override
--- src/main/java/com/note/quarter/drawing/ImageResource.java
+++ src/main/java/com/note/quarter/drawing/ImageResource.java
@@ -1,5 +1,6 @@
 package com.note.quarter.drawing;
 
+import com.note.quarter.MainForm;
 import com.note.quarter.noterest.NoteRestValue;
 import javafx.scene.image.Image;
 
@@ -12,21 +13,21 @@ public class ImageResource {
     private static Map<NoteRestValue, Image> sharpNotes = new HashMap<>();
     private static Map<NoteRestValue, Image> rests = new HashMap<>();
     private static Map<NoteRestValue, Image> additionals = new HashMap<>();
-
-    private static Image staff = new Image(ImageResource.class.getResource("../images/staff.png").toString());
-    private static Image clef = new Image(ImageResource.class.getResource("../images/gclef.png").toString());
-    private static Image barLine = new Image(ImageResource.class.getResource("../images/barLine.png").toString());
-    private static Image meter = new Image(ImageResource.class.getResource("../images/commontime.png").toString());
+    
+    private static Image staff = new Image(MainForm.class.getResource("images/staff.png").toString());
+    private static Image clef = new Image(MainForm.class.getResource("images/gclef.png").toString());
+    private static Image barLine = new Image(MainForm.class.getResource("images/barLine.png").toString());
+    private static Image meter = new Image(MainForm.class.getResource("images/commontime.png").toString());
 
 
     static {
         for(NoteRestValue value : NoteRestValue.values()) {
             String name = value.name().toLowerCase();
-            upNotes.put(value, new Image(ImageResource.class.getResource("../images/notes/up/" + name + ".png").toString()));
-            downNotes.put(value, new Image(ImageResource.class.getResource("../images/notes/down/" + name + ".png").toString()));
-            sharpNotes.put(value, new Image(ImageResource.class.getResource("../images/notes/sharp/" + name + ".png").toString()));
-            rests.put(value, new Image(ImageResource.class.getResource("../images/rests/" + name + ".png").toString()));
-            additionals.put(value,new Image(ImageResource.class.getResource("../images/additionals/"+"additional_"+name+".png").toString()));
+            upNotes.put(value, new Image(MainForm.class.getResource("images/notes/up/" + name + ".png").toString()));
+            downNotes.put(value, new Image(MainForm.class.getResource("images/notes/down/" + name + ".png").toString()));
+            sharpNotes.put(value, new Image(MainForm.class.getResource("images/notes/sharp/" + name + ".png").toString()));
+            rests.put(value, new Image(MainForm.class.getResource("images/rests/" + name + ".png").toString()));
+            additionals.put(value,new Image(MainForm.class.getResource("images/additionals/"+"additional_"+name+".png").toString()));
         }
     }
 
